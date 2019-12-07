191a3598d6666453f487be0812cef30bd6deec15
Jose Molina Colmenero <jmolinacolmenero@protonmail.com>
2019-12-07 20:11:44 +0100

    Add initial tests

--- test/Housteca.js
+++ test/Housteca.js
@@ -1,4 +1,5 @@
 const Housteca = artifacts.require('Housteca');
+const Property = artifacts.require('Property');
 const Loan = artifacts.require('Loan');
 const TestERC20Token = artifacts.require('TestERC20Token');
 const TestERC777Token = artifacts.require('TestERC777Token');
@@ -8,9 +9,45 @@ const { ZERO_ADDRESS } = constants;
 
 
 contract("Housteca", accounts => {
-    beforeEach(async function () {
-        this.erc1820 = await singletons.ERC1820Registry(registryFunder);
-        this.erc777 = await TestERC777Token.new({ from: accounts[0] });
-        this.erc20 = await TestERC20Token.new({ from: accounts[0] });
+    const manager = accounts[0];
+    let erc1820, erc777, erc20, propertyToken, housteca, loan;
+
+    beforeEach(async () => {
+        erc1820 = await singletons.ERC1820Registry(manager);
+        erc777 = await TestERC777Token.new();
+        erc20 = await TestERC20Token.new();
+        propertyToken = await Property.new();
+        housteca = await Housteca.new(propertyToken.address);
+    });
+
+    contract('After contract creation', () => {
+        it('should have the correct root administrator', async () => {
+            const isAdmin = await housteca.isAdmin(manager);
+            assert.isOk(isAdmin);
+        });
+        it('should have the correct Property token address', async () => {
+            const propertyTokenAddress = await housteca._propertyToken();
+            assert.equal(propertyTokenAddress, propertyToken.address);
+        });
+    });
+
+    contract('Manage investors', () => {
+        it('should successfully add, check and delete an investor', async () => {
+            const investor = accounts[1];
+            let isInvestor = await housteca.isInvestor(investor);
+            assert.isNotOk(isInvestor);
+            let tx = await housteca.addInvestor(accounts[1]);
+            isInvestor = await housteca.isInvestor(investor);
+            assert.isOk(isInvestor);
+            truffleAssert.eventEmitted(tx, 'InvestorAdded', event => {
+                return event.investor === investor;
+            });
+            tx = await housteca.removeInvestor(investor);
+            isInvestor = await housteca.isInvestor(investor);
+            assert.isNotOk(isInvestor);
+            truffleAssert.eventEmitted(tx, 'InvestorRemoved', event => {
+                return event.investor === investor;
+            });
+        });
     });
 });
