ee141bf8e37f6c9b5d5a92c839aae6cf21483a23
Jose Molina Colmenero <jmolinacolmenero@protonmail.com>
2019-12-08 14:58:47 +0100

    Add tests for admins and local nodes

--- test/Housteca.js
+++ test/Housteca.js
@@ -4,9 +4,10 @@ const Loan = artifacts.require('Loan');
 const TestERC20Token = artifacts.require('TestERC20Token');
 const TestERC777Token = artifacts.require('TestERC777Token');
 const truffleAssert = require('truffle-assertions');
-const { expectEvent, singletons, constants } = require('@openzeppelin/test-helpers');
-const { ZERO_ADDRESS } = constants;
+const { singletons } = require('@openzeppelin/test-helpers');
 
+
+const BN = web3.utils.toBN;
 
 contract("Housteca", accounts => {
     const manager = accounts[0];
@@ -36,18 +37,46 @@ contract("Housteca", accounts => {
             const investor = accounts[1];
             let isInvestor = await housteca.isInvestor(investor);
             assert.isNotOk(isInvestor);
-            let tx = await housteca.addInvestor(accounts[1]);
+            let tx = await housteca.addInvestor(investor);
+            truffleAssert.eventEmitted(tx, 'InvestorAdded', {investor});
             isInvestor = await housteca.isInvestor(investor);
             assert.isOk(isInvestor);
-            truffleAssert.eventEmitted(tx, 'InvestorAdded', event => {
-                return event.investor === investor;
-            });
             tx = await housteca.removeInvestor(investor);
+            truffleAssert.eventEmitted(tx, 'InvestorRemoved', {investor});
             isInvestor = await housteca.isInvestor(investor);
             assert.isNotOk(isInvestor);
-            truffleAssert.eventEmitted(tx, 'InvestorRemoved', event => {
-                return event.investor === investor;
-            });
+        });
+    });
+
+    contract('Manage admins and local nodes', () => {
+        it('should successfully add, check and delete an admin', async () => {
+            const admin = accounts[5];
+            const level = BN(254);
+            let isAdmin = await housteca.isAdmin(admin);
+            assert.isNotOk(isAdmin);
+            let tx = await housteca.addAdmin(admin, level, 0, 0);
+            truffleAssert.eventEmitted(tx, 'AdminAdded', {admin, level});
+            isAdmin = await housteca.isAdmin(admin);
+            assert.isOk(isAdmin);
+            tx = await housteca.removeAdmin(admin);
+            truffleAssert.eventEmitted(tx, 'AdminRemoved', {admin, level});
+            isAdmin = await housteca.isAdmin(admin);
+            assert.isNotOk(isAdmin);
+        });
+
+        it('should successfully add, check and delete a local node', async () => {
+            const localNode = accounts[5];
+            const level = BN(253);
+            let isLocalNode = await housteca.isLocalNode(localNode);
+            assert.isNotOk(isLocalNode);
+            let tx = await housteca.addAdmin(localNode, level, BN(500), BN(1e16));
+            truffleAssert.eventEmitted(tx, 'AdminAdded', {admin: localNode, level});
+            isLocalNode = await housteca.isLocalNode(localNode);
+            assert.isOk(isLocalNode);
+            tx = await housteca.removeAdmin(localNode);
+            truffleAssert.eventEmitted(tx, 'AdminRemoved', {admin: localNode, level});
+            isLocalNode = await housteca.isLocalNode(localNode);
+            assert.isNotOk(isLocalNode);
         });
     });
 });
