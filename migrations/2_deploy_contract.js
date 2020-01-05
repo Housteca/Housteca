3cd2b6b2826cf00de2c9835639685c195fd0d147
José Molina Colmenero <jmolinacolmenero@protonmail.com>
2020-01-05 16:13:28 +0100

    Deploy and link tokens in ropsten and development

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -10,8 +10,6 @@ module.exports = async (deployer, network, accounts) => {
     if (network === 'development') {
         // In a test environment an ERC777 token requires deploying an ERC1820 registry
         await singletons.ERC1820Registry(accounts[0]);
-        await deployer.deploy(TestERC777Token);
-        await deployer.deploy(TestERC20Token);
     }
 
     await deployer.deploy(Property, {gas: 75e5});
@@ -19,4 +17,14 @@ module.exports = async (deployer, network, accounts) => {
     const instance = await Property.deployed();
     await instance.addMinter(Housteca.address, {from: accounts[0]});
     await instance.transferOwnership(Housteca.address, {from: accounts[0]});
+
+    if (['development', 'ropsten'].includes(network)) {
+        const T20 = await deployer.deploy(TestERC777Token);
+        const T777 = await deployer.deploy(TestERC20Token);
+        const housteca = await Housteca.deployed();
+        const T20Symbol = await T20.symbol();
+        housteca.addToken(T20Symbol, TestERC20Token.address, {from: accounts[0]});
+        const T777Symbol = await T777.symbol();
+        housteca.addToken(T777Symbol, TestERC777Token.address, {from: accounts[0]});
+    }
 };
